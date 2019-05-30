38fac32d6904a908b436c7bbbba90d4dcd64e88f
Kévin MASLOWSKI <k.maslowski@groupeonepoint.com>
2019-05-30 12:54:48 +0200

    first try

---
+++ index.js
@@ -0,0 +1,10 @@
+function Login() {
+    var username = document.login.username.value;
+	var password = document.login.password.value;
+	if (username == "SuperAdmin" && password=="izipassword") {
+        alert("Félicitations ! Tu as réussi à me hacker !\nLa prochaine fois ça ne sera pas aussi facile. :-)");
+	} else { 
+	    alert("Mauvais mot de passe !"); 
+	}
+
+}
